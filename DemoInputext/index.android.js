9cbf2f053557b884f732f2aef62bb8d10fe97556
sunshinewei <1436699184@qq.com>
2017-04-08 10:52:03 +0800

    DrawerLayoutAndroid

--- DemoInputext/index.android.js
+++ DemoInputext/index.android.js
@@ -11,20 +11,38 @@ import {
     Text,
     View,
     TextInput,
-    Touchable,
+    DrawerLayoutAndroid,
 } from 'react-native';
 
 export default class DemoInputext extends Component {
-    render() {
-        return (
-            <SearchInput></SearchInput>
-
+    render(){
+        return(
+            //<SearchInput></SearchInput>
+            <MyDrawerLayout></MyDrawerLayout>
         );
     }
 }
 
-
-
+//DrawerLayoutAndroid的使用
+class MyDrawerLayout extends Component{
+    render(){
+        var navigationView = (
+            <View style={{flex: 1, backgroundColor: '#fff'}}>
+                <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
+            </View>
+        );
+        return(
+                <DrawerLayoutAndroid drawerWidth={300}
+                                     drawerPosition={DrawerLayoutAndroid.positions.Left}
+                                     renderNavigationView={() => navigationView}>
+                    <View style={{flex: 1, alignItems: 'center',justifyContent:'center'}}>
+                        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>主界面</Text>
+                        <Text style={{margin: 10, fontSize: 15, textAlign: 'right'}}>World!</Text>
+                    </View>
+                </DrawerLayoutAndroid>
+        );
+    }
+}
 
 //TextInput的使用
 class SearchInput extends Component{
